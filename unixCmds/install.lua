9c250b80ab56eefcca1a564f56bea72804e16eab
Jonathan Rotter <hydrogen31415@gmail.com>
2020-07-08 13:45:59 +0200

    installer

---
+++ unixCmds/install.lua
@@ -0,0 +1,45 @@
+
+-- run inside /disk
+
+function main()
+    if not fs.isDir('/bin') then
+        fs.makeDir('/bin')
+        print('Created /bin')
+    end
+
+    if not fs.isDir('bin') then
+        print('Error: Can not find bin/ in disk')
+        return
+    end
+
+    local src = shell.resolve('bin')
+
+    local files = fs.list(src)
+
+    local counter = 0
+
+    for i,v in ipairs(files) do
+        fs.copy(src .. '/' .. v, '/bin/' .. v)
+        print('Copied bin/' .. v)
+        counter = counter + 1
+    end
+
+    local f = io.open('/startup', 'a')
+
+    if not f then 
+        print('Could not open file /startup')
+    end
+
+    f:write('\n')
+    f:write('shell.setPath(shell.path() .. ":/bin")\n')
+
+    f:close()
+
+    print('Done, installed ', counter, ' files')
+
+    print('Rebooting in 3 seconds...')
+    sleep(3)
+    os.reboot()
+end
+
+main()
